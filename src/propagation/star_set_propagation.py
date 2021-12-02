439ed42cd1b11957fd6bcdf0f8afabc56ccd2775
Philipp Roytburg <48437444+philipproy@users.noreply.github.com>
2021-12-02 11:25:15 +0100

    StarSet propagation plan

--- src/propagation/star_set_propagation.py
+++ src/propagation/star_set_propagation.py
@@ -1,15 +1,33 @@
 from src.propagation.abstract_domain_propagation import AbstractDomainPropagation
+import numpy as np
+from typing import Optional
 
 # TODO
 class StarSetPropagation(AbstractDomainPropagation):
-    pass
+    def calc_bounds(self, input_constraints: np.array, from_layer: int = 1) -> bool:
+        raise NotImplementedError(
+            f"calc_bounds(...) not implemented in {self.__name__}"
+        )
+
+    # TODO: for star sets should not be very different from deeppoly
+    def merge_current_bounds_into_forced(self):
+        pass
+
+    # TODO: which technique do we use?
+    def largest_error_split_node(
+        self, output_weights: np.array = None
+    ) -> Optional[tuple]:
+        pass
 
 
 # TODO
 class StarSetForwardPropagation(StarSetPropagation):
-    pass
+    # TODO
+    def calc_bounds(self, input_constraints: np.array, from_layer: int = 1) -> bool:
+        pass
 
 
-# TODO
 class StarSetBackwardPropagation(StarSetPropagation):
-    pass
+    # TODO
+    def calc_bounds(self, input_constraints: np.array, from_layer: int = 1) -> bool:
+        pass
